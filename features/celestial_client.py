a86f884a03593acae9f54463d7f55eeb45887e9d
Adam <PseudoDesign@users.noreply.github.com>
2019-05-28 22:51:29 -0600

    passing test

--- features/celestial_client.py
+++ features/celestial_client.py
@@ -3,6 +3,9 @@ import subprocess
 
 
 def rootfs_install(rootfs_file, device_node, block_size_KB=10):
+    """
+    Install rootfs_file into device_node
+    """
     result = subprocess.run([
         'dd',
         'if={}'.format(rootfs_file),
