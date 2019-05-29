671a54d914cad012743675cf683697533a0d3e8c
Adam <PseudoDesign@users.noreply.github.com>
2019-05-28 22:50:21 -0600

    Test passing

--- features/celestial_client.py
+++ features/celestial_client.py
@@ -1,3 +1,12 @@
-# Wrappers for the Cestial scripts
-def rootfs_install(rootfs_file, device_node):
-    pass
+# Wrappers for the Cestial client scripts
+import subprocess
+
+
+def rootfs_install(rootfs_file, device_node, block_size_KB=10):
+    result = subprocess.run([
+        'dd',
+        'if={}'.format(rootfs_file),
+        'of={}'.format(device_node),
+        'bs={}K'.format(block_size_KB)
+    ])
+    assert result.returncode == 0
