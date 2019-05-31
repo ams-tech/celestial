3e931b71b011917f057bca9c8d6ac4a661126f72
Adam <PseudoDesign@users.noreply.github.com>
2019-05-30 23:06:33 -0600

    Reconfigured to use docker compose-ed remote interpreter

--- features/steps/rootfs_install.py
+++ features/steps/rootfs_install.py
@@ -4,16 +4,17 @@ import celestial.client.rootfs
 import utils
 import filecmp
 import celestial
+from celestial.strings import Filesystems
 
 
-@given('a rootfs file formatted with {rootfs_format}')
+@given(u'a rootfs file formatted with {rootfs_format}')
 def step_impl(context, rootfs_format):
     # Generate a small ext3 formatted file
-    if rootfs_format == "ext2":
+    if rootfs_format == Filesystems.EXT2:
         context.rootfs_file = utils.make_ext2()
-    elif rootfs_format == "ext3":
+    elif rootfs_format == Filesystems.EXT3:
         context.rootfs_file = utils.make_ext3()
-    elif rootfs_format == "ext4":
+    elif rootfs_format == Filesystems.EXT4:
         context.rootfs_file = utils.make_ext4()
     else:
         raise ValueError("Unsupported rootfs type")
--- features/steps/samples.py
+++ features/steps/samples.py
@@ -7,6 +7,7 @@ STEPS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 @given("the sample {subtype} file {filename}")
 def step_impl(context, subtype, filename):
     """
+    sets the "context.sample_{subtype}_file" attribute to "{filename}"
     :param filename:
     :param subtype:
     :type context: behave.runner.Context
